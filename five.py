8c1b03192f86191f8f979d48bc994333e1a05d66
0dsabella <42749703+0dsabella@users.noreply.github.com>
2020-05-05 17:22:56 -0500

    Update five.py

--- five.py
+++ five.py
@@ -1,4 +1,4 @@
-"""  
+""" 
     For this problem you will modify a function that creates a password
     the password needs to include features from the input list
     that contains information about the person.
@@ -7,16 +7,10 @@
     include 2-4 numbers
     be of lenght 18-36 characters
     NOTE: 0 points if when you run the code again, the password is similar.
-          Every time you run the code you should get a unique string. 
+          Every time you run the code you should get a unique string.
 """
 import random
-
-def passwordGenerator(personal_list):
-    password = n00b1234
-    return password
-
-
-
-if __name__ == "__main__":
-    mylist = [12,"BOB",-2,"Amy",["David","Jona","Timmothy"],"Burt"]
-    print(passwordGenerator(mylist))
+import string
+def passwordGenerator(size = 8, chars=string.ascii_letters + string.digits + string.punctuation):
+    return ''.join(random.choice(chars) for _ in range(size))
+print(passwordGenerator(int(input('How many characters are there in your password?'))))
